50e995ffa4cf32ee2e78d3eb4d04c86cc06d2310
兔一 <rabbitzzc@gmail.com>
2020-09-15 21:33:50 +0800

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: true,
+    semi: true,
+    trailingComma: "none",
+    bracketSpacing: true,
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'ignore'
+}
---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import babel from "rollup-plugin-babel"
+
+// 压缩
+import {
+    terser
+} from "rollup-plugin-terser"
+
+// 输出文件夹清除
+import clear from "rollup-plugin-clear"
+const path = require('path')
+
+const resolve = function (filePath) {
+    console.log(path.join(__dirname,  filePath))
+    return path.join(__dirname,  filePath)
+}
+export default {
+    input: resolve('/src/index.js'), // 入口文件
+    output: [{ // 出口文件
+        file: resolve('dist/index.min.js'),
+        format: 'umd',
+        name: 'npm-lib',
+        plugins: [
+            clear({
+                targets: ["dist"]
+            }),
+            babel({
+                exclude: 'node_modules/**'
+            }), terser()
+        ]
+    }],
+    external: []
+}
